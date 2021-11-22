6d8fa6e1c6d50c7997f457296cfb089b42afd6e1
BorzavariDaniel <dani.borzavari@gmail.com>
2021-11-22 18:31:27 +0100

    New interface inside Logic

---
+++ I8SSYF_HFT_2021221.Logic/ICarLogic.cs
@@ -0,0 +1,22 @@
+﻿using I8SSYF_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I8SSYF_HFT_2021221.Logic
+{
+    interface ICarLogic
+    {
+        void Create(Car car);
+        IQueryable<Car> ReadAll();
+        void Update(Car car);
+        void Delete(int carId);
+
+        double AveragePrice();
+
+        IEnumerable<KeyValuePair<string, double>>
+            AveragePricesByBrands();
+    }
+}
