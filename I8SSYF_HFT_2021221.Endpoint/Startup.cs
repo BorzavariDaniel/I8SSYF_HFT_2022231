759041bcfca21035981281198d15f6d28310c3aa
BorzavariDaniel <dani.borzavari@gmail.com>
2022-05-23 23:53:59 +0200

    Endpoint, swagger

--- I8SSYF_HFT_2021221.Endpoint/Startup.cs
+++ I8SSYF_HFT_2021221.Endpoint/Startup.cs
@@ -1,3 +1,4 @@
+using Castle.Core.Configuration;
 using I8SSYF_HFT_2021221.Data;
 using I8SSYF_HFT_2021221.Logic;
 using I8SSYF_HFT_2021221.Repository;
@@ -6,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,6 +18,13 @@ namespace I8SSYF_HFT_2021221.Endpoint
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -28,6 +38,8 @@ namespace I8SSYF_HFT_2021221.Endpoint
             services.AddTransient<IEngineRepository, EngineRepository>();
 
             services.AddTransient<CarDbContext, CarDbContext>();
+
+            services.AddSwaggerGen(x => x.SwaggerDoc("v1", new OpenApiInfo { Title = "I8SSYF_HFT_2021221.Endpoint", Version = "v1" }));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -35,6 +47,8 @@ namespace I8SSYF_HFT_2021221.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/vi/swagger.json", "I8SSYF_HFT_2021221.Endpoint v1"));
             }
 
             app.UseRouting();
